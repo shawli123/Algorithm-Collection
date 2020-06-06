0f67990c8ac3a8ad4703334ee1e8145c902f7714
lixiaowei30 <lixiaowei9@jd.com>
2020-06-06 21:39:08 +0800

    feat: 和为s的两个数字

---
+++  和为s的两个数字.js
@@ -0,0 +1,39 @@
+/**
+一、题目描述：
+输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+
+二、示例
+输入：nums = [2,7,11,15], target = 9
+输出：[2,7] 或者 [7,2]
+
+三、解题思路：
+双指针查找法
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var result = [];
+    let i = 0; j = nums.length - 1;
+    while(i < nums.length && j >= 0) {
+        var sum = nums[i] + nums[j];
+        if (sum === target) {
+            result = [nums[i], nums[j]];
+            break;
+        }
+        if (sum > target) {
+            j--;
+        } else if (sum < target) {
+            i++;
+        }
+    }
+    return result;
+};
+
+
+module.exports = {
+  twoSum : twoSum
+};
